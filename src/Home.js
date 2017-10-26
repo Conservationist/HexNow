a0cc00eb80a5d9001922967c407f525b695d0a44
Conservationist <conorosullivanmitchell@gmail.com>
2017-10-27 00:07:14 +0300

    lots of changes, mainly added support for the firebase DB, background image request, and added components

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Background from './components/background/Background';
+import Quote from './components/quote/quote';
 import quotes from './quotes.json';
+import CenterInfo from './components/center/centerinfo';
+import firebase from 'firebase';
+// LETS GO.
+
 class Home extends Component {
   constructor(props){
     super(props);
@@ -9,20 +14,28 @@ class Home extends Component {
       loading: 'true',
     }
   }
+  componentWillMount(){
+    let config = {
+      apiKey: "AIzaSyCEiObiNI-GR9JIHGf_xNgbRrZkM3xvKgg",
+      authDomain: "hexnow-2004d.firebaseapp.com",
+      databaseURL: "https://hexnow-2004d.firebaseio.com/"
+    };
+    firebase.initializeApp(config);
+  }
   componentDidMount(){
-    axios.get(`https://source.unsplash.com/1600x900/?coffee`)
+    axios.get(`https://source.unsplash.com/1600x900/?ocean`)
       .then(res =>{
         let img = res.request.responseURL
-        this.setState({loading:'false', ImageURL:img})
+        const rannum = Math.floor(Math.random() * 102)
+        this.setState({loading:'false', ImageURL:img, QuoteBody: quotes.quotes[rannum].quote, QuoteAuthor: quotes.quotes[rannum].author})
       });
-      console.log(quotes);
-      console.log(Math.floor(Math.random() * 102));
   }
   render() {
-    console.log(this.state.quoteBody);
     return (
       <div className="App">
         <Background img={this.state.ImageURL}/>
+        <Quote quoteBody={this.state.QuoteBody} quoteAuthor={this.state.QuoteAuthor}/>
+        <CenterInfo/>
       </div>
     );
   }
