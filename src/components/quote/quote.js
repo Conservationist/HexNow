9d6a3e9797b91dd207cd0a5d84c60f1e3b5a307c
Conservationist <conorosullivanmitchell@gmail.com>
2017-10-28 18:04:03 +0300

    minor changes / reworks

--- src/components/quote/quote.js
+++ src/components/quote/quote.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Colors, Sizes} from '../_themes/text.style';
 import {DIV, P} from './quote.style'
 
 function Quote({quoteBody, quoteAuthor}){
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './Home';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+let config = {
+    apiKey: "AIzaSyCEiObiNI-GR9JIHGf_xNgbRrZkM3xvKgg",
+    authDomain: "hexnow-2004d.firebaseapp.com",
+    databaseURL: "https://hexnow-2004d.firebaseio.com",
+    projectId: "hexnow-2004d",
+    storageBucket: "hexnow-2004d.appspot.com",
+    messagingSenderId: "1068076272402"
+}
+firebase.initializeApp(config);
+
+ReactDOM.render(<Home/>, document.getElementById('root'));
 registerServiceWorker();
